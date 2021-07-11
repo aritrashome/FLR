2f956a8e66ad6a1575a6c01e6dc9d53d67f88797
aritrashome <43993552+aritrashome@users.noreply.github.com>
2021-07-11 19:48:23 +0530

    Update generat_prec.py

--- generat_prec.py
+++ generat_prec.py
@@ -12,18 +12,13 @@ df = pd.read_csv('Karnataka_data.csv')
 df = df[df['yod']<=2013]
 df = df.drop(['prec','label','land_cover_5yr','land_cover'],axis=1)
 
-year = 2003
-df['prec_'+str(year)] = 0
-for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
-    filepath = 'Prec/CHELSA_prec_'+str(year)+'_'+month+'_V1.2.1.tif'
-    src = rasterio.open(filepath,)
-    prec = src.read(1)
-    print(month,year, end=' ')
-    for idx in range(len(df)):
-        row, col = df['row'][idx], df['col'][idx]
-        try:
-            df['prec_'+str(year)][idx] += prec[row][col]
-        except:
-            df['prec_'+str(year)][idx] += 0
-
-df.to_csv('Karnataka_final.csv',index=False)
+#year = 2003
+for year in range(2010,2014):
+    df['prec_'+str(year)] = 0
+    for month in ['01','02','03','04','05','06','07','08','09','10','11','12']:
+        filepath = 'Prec/CHELSA_prec_'+str(year)+'_'+month+'_V1.2.1.tif'
+        src = rasterio.open(filepath,)
+        prec = src.read(1)
+        print(month,year, end=' ')
+        df['prec_'+str(year)] += df.apply(lambda row : prec[row['row']][row['col']] , axis=1)
+    df.to_csv('Karnataka_final.csv',index=False)
